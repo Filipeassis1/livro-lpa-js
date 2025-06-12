d94205fec47d74d5eb1f5f9c7561ca2399464775
Filipe Assis <filipeassisr@gmail.com>
2025-06-11 21:10:51 -0300

    1.2  cap2

---
+++ cap2/exercicios/jsex/ex1.js
@@ -0,0 +1,29 @@
+//captar os dados do form e h3 como resposta
+
+const frm = document.querySelector("form");
+const resp1 = document.querySelector("h3");
+const resp2 = document.querySelector("h2");
+
+
+
+//tratar os dados do forms
+
+frm.addEventListener("submit", (e)=>{
+
+
+    //obter os valores 
+
+    const medicameto = frm.medicine.value;
+    const valor = frm.price.value;
+
+    const preco = Math.floor( valor );
+    // const cents = preco % 100;
+
+
+    resp1.innerText = ` Promocao de ${medicameto}`; 
+    resp2.innerText = `Leve 2 por apenas ${preco}`
+
+    e.preventDefault();
+
+
+})
